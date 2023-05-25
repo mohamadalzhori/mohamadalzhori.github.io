9ee1e775f028a81ba25f1ef2e1321d8f1378ba23
Mohamad Alzhori <mohamadalzhori01@gmail.com>
2023-05-26 02:03:26 +0300

    initial

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,3 +106,16 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__data, .home__social, .contact__container, .footer__container')
+sr.reveal('.home__image', {origin: 'bottom'})
+sr.reveal('.about__data, .skills__data', {origin: 'left'})
+sr.reveal('.about__image, .skills__content', {origin: 'right'})
+sr.reveal('.projects__card', {interval: 100 })
