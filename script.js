0e9ffefbaaf4a41a1a39c5b69100ac5d77299007
Mohamad Alzhori <mohamadalzhori01@gmail.com>
2023-11-03 18:46:07 +0200

    Added Highlights

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ var Typer = {
           
           'Hola! I am Mohamad Alzhori. \n\n' +
 
-          'I am a student at the Lebanese American University, pursuing a Bachelor of Engineering in Electrical Engineering, with an expected graduation date of May 2025.\n\n'+
+          'I am a student at the Lebanese American University, pursuing a Bachelor of Engineering in <span id="a">Electrical</span> Engineering, with an expected graduation date of <span id="a">May 2025</span>.\n\n'+
           
-          'My passion for electrical engineering sparked my journey, and I have honed my skills in programming and software development. \n\n' +
+          'My passion for electrical engineering <span id="a">sparked</span> my journey, and I have honed my skills in programming and software development. \n\n' +
           
-          'I am driven by the desire to consistently improve and have a meaningful impact in the world.\n\n' + 
+          'I am driven by the desire to <span id="a">consistently</span> improve and have a meaningful impact in the world.\n\n' + 
           
-          'I am also proud to be a recipient of the prestigious USAID Higher Education Scholarship Program (HES) at the Lebanese American University, which has provided me with invaluable educational opportunities.\n\n' + 
+          'I am also proud to be a recipient of the prestigious <span id="a">USAID</span> Higher Education Scholarship Program (HES) at the Lebanese American University, which has provided me with invaluable educational opportunities.\n\n' + 
           
-          'I am eagerly seeking software engineering opportunities that will allow me to boost and apply my knowledge and skills to solve real-world problems.\n\n' + 
+          'I am eagerly seeking <span id="a">software</span> engineering opportunities that will allow me to boost and apply my knowledge and skills to solve real-world problems.\n\n' + 
           
           'If you are interested in learning more about how I can contribute to your organization, please feel free to reach out to me via <a href="https://www.linkedin.com/in/mohamadalzhori">LinkedIn</a> or <a href="mailto:mohamadalzhori01@gmail.com">mohamadalzhori01@gmail.com</a> \n\n' +
           
